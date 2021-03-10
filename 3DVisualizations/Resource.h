a0b233afdb4b5efd1dee6c7c16c6dcd4871bfee1
Brendan Burkhart <brendan.f.burkhart@gmail.com>
2021-03-10 13:40:12 -0500

    3D-Engine -> 3DVisualizations

--- 3DVisualizations/Resource.h
+++ 3DVisualizations/Resource.h
@@ -3,19 +3,19 @@
 // Used by 3DEngine.rc
 //
 
-#define IDS_APP_TITLE			103
+#define IDS_APP_TITLE			      103
 
-#define IDR_MAINFRAME			128
-#define IDD_MY3DENGINE_DIALOG	102
-#define IDD_ABOUTBOX			103
-#define IDM_ABOUT				104
-#define IDM_EXIT				105
-#define IDI_MY3DENGINE			107
-#define IDI_SMALL				108
-#define IDC_MY3DENGINE			109
-#define IDC_MYICON				2
+#define IDR_MAINFRAME			      128
+#define IDD_MY3DVISUALIZATIONS_DIALOG 102
+#define IDD_ABOUTBOX			      103
+#define IDM_ABOUT				      104
+#define IDM_EXIT				      105
+#define IDI_MY3DVISUALIZATIONS	      107
+#define IDI_SMALL				      108
+#define IDC_MY3DVISUALIZATIONS        109
+#define IDC_MYICON				      2
 #ifndef IDC_STATIC
-#define IDC_STATIC				-1
+#define IDC_STATIC				      -1
 #endif
 // Next default values for new objects
 //
